9ab5d895c0d63c53878ead79b7984620838fd2fb
Kenny Yang <kennyyang2019@gmail.com>
2024-09-16 16:32:21 -0400

    finished assignment

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-require('dotenv').config();
+// require('dotenv').config();
+const API_KEY = "q2X77WuKBHvgvlq82pffWPnsjbcSROnn";
 
-// Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+const inputField = document.querySelector("#queryInput");
+const submitBtn = document.querySelector("#submitBtn");
+const container = document.querySelector(".container");
+
+submitBtn.addEventListener("click", () => {
+    container.innerHTML = ''
+	const queryValue = inputField.value;
+	const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${queryValue}&limit=25&offset=0&rating=g&lang=en`;
+	fetch(endpoint)
+		.then((res) => res.json())
+		.then((data) => {
+			const gifs = data.data;
+			gifs.forEach((gif) => {
+				let id = gif.id;
+				let url = `https://media.giphy.com/media/${id}/giphy.gif`;
+				const img = document.createElement("img");
+				img.src = url;
+				container.appendChild(img);
+			});
+		});
+});
